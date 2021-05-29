86e1404630ed6cc8d7b7183db994c8ed9f049e7e
Fernando Coda <fcoda.96@gmail.com>
2021-05-29 06:43:22 -0300

    Reimplemented the whole GUI in GTK+ (WIP)

--- README.md
+++ README.md
@@ -1,41 +1,27 @@
-# GUI Scientific Calculator
+# Simple Scientific Calculator
 Written from scratch in C
 
 ## About the project
-The GUI works with the **_libui_** C graphic library from Pietro Gagliardi  
-All the thanks to his project, link to [his git](https://github.com/andlabs/libui)  
-About the core, it's all written in 3 libraries, one for the math functions, other for processing the expressions, and the third with some common functions like string ones.
+The GUI has been written in GTK+  
+About the core, it's all written in 3 libraries, one for the math functions, other for processing the expressions, and the third with some common functions like string handling.
 
 ## Building (linux)
-To build just use the makefile in the project main folder
+To build the project you can use cmake
 ```
+$ mkdir -p build && cd build
+$ cmake ..
 $ make build
 ```
-### Installing
-To install, root privileges are not required, since the install dir is in the user tree.  
-Anyway, to properly set the library path into the system (optional), the only way is via `/etc/ld.so.conf`, for which root permissions are required (see below).  
-Install with
+
+Also some makefiles are left from previous attempts
+
+## Run
+You can run the GUI simply by double clicking the generated executable or by command line  
 ```
-$ make install
+build $ ./sscalc-gui
+```
+Also the program can be run from the terminal  
+```
+build $ ./sscalc
 ```
 
-### Run
-You can run the GUI simply with the desktop entry that is put into `/home/user/.local/share/applications`, accessible from your desktop menu.  
-Also the program can be run from the terminal (if the path is properly set, see below), both in gui and terminal mode.  
-To run the gui:
-```
-$ calc-ui
-```
-and the console mode:
-```
-$ calc
-```
-
-### Configuring the PATH
-To properly set the libraries and the PATH environment variable, you need to run `make configure`. It will ask for setting a line into your `~/.bashrc`, and then add the library directory to `/etc/ld.so.conf.d/user.config` (root are required). Be careful on this step, since it will override this file, even if it already exists.  
-Also it is recommended moving the PATH export from `~/.bashrc` to the more general `~/.profile` file. But be aware that, if you do so, the changes only will apply on the next login.
-
-
-## Documentation
-Any problem within linking the shared libraries, more info in:
-https://www.cprogramming.com/tutorial/shared-libraries-linux-gcc.html
--- src/main.c
+++ src/main.c
@@ -1,15 +1,98 @@
-#include "mainWindow.h"
+//
+// Created by feer on 19/5/21.
+//
 
-int main(int argc, char *argv[])
+#include <gtk/gtk.h>
+#include <gio/gio.h>
+#include <gdk/gdkkeysyms.h>
+#include <gtk/gtkbuildable.h>
+#include <glib.h>
+
+static struct window {
+	GtkWidget *window;
+	GtkEntryBuffer *buffer_in;
+	GtkTextBuffer *buffer_out;
+	GtkEntry *text_in;
+	GtkTextView *text_out;
+} win;
+
+void setupGUI();
+void set_pointers(GtkBuilder *builder);
+void set_signals(GtkBuilder *builder);
+static gboolean keys_handler(GtkWidget *widget, GdkEventKey *event, gpointer data);
+
+int main (int argc, char *argv[])
 {
-	mainWindow_t mainWindow;
-	uiConfigure();
-	uiShowWindow_mainWindow(&mainWindow);
-	uiMain();
-	DBGPRNT("after uiMain()\n");
-	
-	uiUninit();
-	DBGPRNT("after uiUninit()\n");
-	
-	return 0;
-} 
+	gtk_init (&argc, &argv);
+
+	setupGUI();
+
+	gtk_main ();
+
+	return (0);
+}
+
+void setupGUI()
+{
+	GtkBuilder  *builder = gtk_builder_new_from_resource ("/org/gtk/SSCalc/sscalc.ui");
+
+	set_pointers(builder);
+	set_signals(builder);
+
+	g_object_unref (G_OBJECT (builder));
+
+	gtk_widget_show (win.window);
+}
+
+void set_signals(GtkBuilder *builder)
+{
+	g_signal_connect(win.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(win.window, "key_press_event", G_CALLBACK(keys_handler), NULL);
+
+	// Load each callback defined in the .glade file into the gtk API
+	gtk_builder_connect_signals(builder, NULL);
+}
+
+void set_pointers(GtkBuilder *builder)
+{
+	win.window     = GTK_WIDGET(   gtk_builder_get_object(builder, "window"));
+	win.text_in    = GTK_ENTRY(       gtk_builder_get_object(builder, "text_in"));
+	win.text_out   = GTK_TEXT_VIEW(   gtk_builder_get_object(builder, "text_out"));
+	win.buffer_in  = GTK_ENTRY_BUFFER(gtk_builder_get_object(builder, "buffer_in"));
+	win.buffer_out = GTK_TEXT_BUFFER( gtk_builder_get_object(builder, "buffer_out"));
+}
+
+
+void on_button_equal_clicked ()
+{
+	static char i = '0';
+	const gchar *in = gtk_entry_get_text(win.text_in);
+
+	gtk_text_buffer_set_text(win.buffer_out, in, -1);
+	gtk_entry_buffer_set_text(win.buffer_in, &i, 1);
+	g_print("equal clicked\n");
+	++i;
+}
+
+
+void on_menu_discard_activate ()
+{
+	gtk_text_buffer_set_text(win.buffer_out, "", -1);
+	gtk_entry_buffer_delete_text(win.buffer_in, 0, -1);
+}
+
+void on_text_in_changed (GtkEditable *text_in)
+{
+//	const gchar *in = gtk_entry_get_text(win.text_in);
+	g_print("text in changed\n");
+}
+
+static gboolean keys_handler(GtkWidget *widget, GdkEventKey *event, gpointer data)
+{
+	if (event->keyval == GDK_KEY_Escape) {
+		on_menu_discard_activate();
+		return TRUE;
+	}
+	g_print("key pressed\n");
+	return FALSE; // do not handle this, and let other handler do the job
+}
